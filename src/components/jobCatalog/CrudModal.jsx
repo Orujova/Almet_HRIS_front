2e6a314fd022ab7c2d7379b727305fc20b9b57f3
Orujova Narmin <nerminorucova2004@gmail.com>
2025-10-23 15:21:49 +0400

    Update

--- src/components/jobCatalog/CrudModal.jsx
+++ src/components/jobCatalog/CrudModal.jsx
@@ -311,7 +311,7 @@ export default function CrudModal({ context, darkMode }) {
 
   return (
     <div className="fixed inset-0 bg-black bg-opacity-50 flex items-center justify-center p-4 z-50">
-      <div className="bg-white dark:bg-almet-cloud-burst rounded-lg max-w-lg w-full max-h-[90vh] overflow-y-auto shadow-xl border border-gray-200 dark:border-almet-comet">
+      <div className="bg-white dark:bg-almet-cloud-burst rounded-lg max-w-lg w-full  shadow-xl border border-gray-200 dark:border-almet-comet">
         
         {/* Header */}
         <div className="px-4 py-3 border-b border-gray-200 dark:border-almet-comet">
